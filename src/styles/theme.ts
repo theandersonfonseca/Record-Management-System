6757d5ed818027b1e320bdc4fe777c0e4d435747
Anderson <andersonfonseca1999@hotmail.com>
2021-07-23 18:05:05 -0300

    Add initial config

---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+export default {
+  font: {
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.2rem',
+    },
+  },
+  colors: {
+    white: '#F8F8F8',
+    black: '#121214',
+    darkGray: '#333333',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+  },
+} as const;
