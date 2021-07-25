c704c7d1c1b0de10a890224762e316a9fcd0e4ad
Anderson <andersonfonseca1999@hotmail.com>
2021-07-25 16:41:25 -0300

    Add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
+
+import Header from 'components/Header';
+import Register from 'components/Register';
+import Footer from 'components/Footer';
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Register />
+      <Footer />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
